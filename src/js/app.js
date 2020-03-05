2f2502a432353911121e702df4019e5c036fba37
brandonroth02 <59844743+brandonroth02@users.noreply.github.com>
2020-03-04 22:19:05 -0500

    added cookie counting function

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,6 @@
 var cookiecount = 0;
 function add(){
     cookiecount = cookiecount +1
+    document.getElementById('text').value = cookiecount;
+    document.title = cookiecalamity + " Cookies";
 }
