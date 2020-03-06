d9d18074098010a28bbb25d4683d2389635860cd
brandonroth02 <59844743+brandonroth02@users.noreply.github.com>
2020-03-05 20:56:31 -0500

    clicker stopped working

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,18 @@
+    var cookiecount = 0;
+    
+    document.title = cookiecalamity + " Cookies";
+    document.getElementById('ammountAutoClick').innerText = "You have" + autoClick + "Auto Click Cursors";
+    document.getElementById('costAutoClick').innerHTML = ((autoClick+1) * 12) + " Cookies";
 
-var cookiecount = 0;
 function add(){
     cookiecount = cookiecount +1
-    document.getElementById('text').value = cookiecount;
+    document.getElementById("text").value = cookiecount;
     document.title = cookiecalamity + " Cookies";
 }
 
-function update(){}
 
-var cookiecount = 0;
-var autoClick = 0;
 
-function timer(){
-    cookiecount = cookiecount + autoClick;
-    update()
-}
-setInterval(timer, 1000)
+
+
+
+
